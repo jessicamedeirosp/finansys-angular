27c045bec13148971f127e4a48b062cc115c6a16
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-02-09 14:23:45 -0300

    adding base method to CategoryForm

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentyAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessage: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuild: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrencyAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  // PRIVATE METHOD
+
+  private setCurrencyAction() {
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentyAction = 'new';
+    else
+      this.currentyAction = 'edit';
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuild.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentyAction === 'edit')
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      )
+      .subscribe(
+        category => {
+          this.category = category;
+          this.categoryForm.patchValue(category) // binds loaded category data to CategoryForm
+        },
+        error => alert('Ocorreu um erro no servidor, tente mais tarde.')
+      );
+  }
+
+  private setPageTitle() {
+    if (this.currentyAction === 'new')
+      this.pageTitle = 'Cadastro de  uma Nova Categoria';
+    else {
+      const categoryName = this.category.name || '';
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
 }
