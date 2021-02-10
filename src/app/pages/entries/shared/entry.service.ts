641670f54ba4f7e7a6696d717da72f9a72868048
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-02-09 21:25:12 -0300

    adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,9 +1,10 @@
+import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Entry } from './entry.model';
 
-import { map, catchError } from 'rxjs/operators'
+import { map, catchError, flatMap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ import { map, catchError } from 'rxjs/operators'
 export class EntryService {
   private apiPath: string = "api/entries";
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -29,18 +32,33 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+      })
     );
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
+
   }
 
   delete(id: null): Observable<null> {
